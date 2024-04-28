9928a6b0018f6c2937bda174a25385e9398d6c4d
shivamgondSuj <136161872+shivamgond420@users.noreply.github.com>
2024-04-28 07:57:31 -0700

    Order problem fix

--- Schema/OrderSchema.js
+++ Schema/OrderSchema.js
@@ -5,7 +5,6 @@ const OrderSchema = new mongoose.Schema({
     {
       price: {
         type: Number,
-        required: true,
       },
 
       Quantity: {
@@ -14,55 +13,43 @@ const OrderSchema = new mongoose.Schema({
 
       ProductId: {
         type: Number,
-        required: true,
       },
       title: {
         type: String,
-        required: true,
       },
       thumbnail: {
         type: String,
-        required: true,
       },
     },
   ],
   UserId: {
     type: String,
-    required: true,
   },
   Add: [
     {
       email: {
         type: String,
-        required: true,
       },
       CardHolder: {
         type: String,
-        required: true,
       },
       Card_Details: {
         type: String,
-        required: true,
       },
       credit_expiry: {
         type: String,
-        required: true,
       },
       credit_cvc: {
         type: String,
-        required: true,
       },
       billing_address: {
         type: String,
-        required: true,
       },
       billing_state: {
         type: String,
-        required: true,
       },
       billing_zip: {
         type: String,
-        required: true,
       },
     },
   ],
